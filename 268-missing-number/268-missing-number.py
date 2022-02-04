bc216a8107fedaf887d8a57f8238e6be3e5725cf
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-03 20:50:34 -0500

    Time: 119 ms (66.33%), Space: 14.8 MB (46.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,16 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Using Sort #########
         # nums.sort()
         # for i in range(len(nums)):
         #     if i!=nums[i]:
         #         return i
         # return i+1
-        hashmap={}
-        for i in range(len(nums)):
-            hashmap[nums[i]]=i
-        #print(hashmap)
-        for i in range(len(nums)):
-            if i not in hashmap:
-                return i
-        return i+1
+        
+        # Using hashmap##########
+        # hashmap={}
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]]=i
+        # #print(hashmap)
+        # for i in range(len(nums)):
+        #     if i not in hashmap:
+        #         return i
+        # return i+1
+        
+        # Using Sum########## gaussian formula : n(n+1)/2
+        original_sum= len(nums)*(len(nums)+1)//2
+        actual_sum=sum(nums)
+        return original_sum-actual_sum

a22f7e5dd2c206cedf724b43f8e2e90c2284538e
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-03 19:19:02 -0500

    Time: 52 ms (35.98%), Space: 18.7 MB (7.26%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
+    
+    
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        inorder_trav=self.inorderTraversal(root)
+        if len(set(inorder_trav))!=len(inorder_trav):
+            return False
+        return True if inorder_trav==sorted(inorder_trav) else False
+        

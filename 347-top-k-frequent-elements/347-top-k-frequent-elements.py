b1f34bdbcbf3a43c61e08991f5ba6048d27e8370
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-04 21:02:10 -0500

    Time: 88 ms (77.84%), Space: 16.7 MB (74.12%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,12 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        maxlist,answer=[],[]
-        counts=collections.Counter(nums)
-        for key,value in counts.items():
-            maxlist.append([value,key])
-        maxlist.sort(key=lambda x : x[0],reverse=True)
-        for i in range(k):
-            answer.append(maxlist[i][1])
-        return answer
+        # Noob approach ############
+        # maxlist,answer=[],[]
+        # counts=collections.Counter(nums)
+        # for key,value in counts.items():
+        #     maxlist.append([value,key])
+        # maxlist.sort(key=lambda x : x[0],reverse=True)
+        # for i in range(k):
+        #     answer.append(maxlist[i][1])
+        # return answer
         
+        
+        # using nlargest function of heap #######
+        if k==len(nums):
+            return nums
+        
+        counts=collections.Counter(nums)
+        return nlargest(k,counts.keys(),key=counts.get)

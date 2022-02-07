abf0d41f566a7440d5f99ee785426bb56cda83e0
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-07 16:36:09 -0500

    Time: 123 ms (9.85%), Space: 17 MB (5.96%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        if len(intervals)<=1:
+            return True
+        intervals.sort(key=lambda x:x[0])
+        temp=[intervals[0]]
+        for start,end in intervals[1:]:
+            if temp[-1][1]>start:
+                return False
+            else:
+                temp.append([start,end])
+        return True

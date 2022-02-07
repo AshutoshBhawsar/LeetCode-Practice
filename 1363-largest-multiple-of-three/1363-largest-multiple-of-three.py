21a8f2079e41d1bd85dac24743af3fdac6c2db3e
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-07 17:51:22 -0500

    Time: 4828 ms (5.00%), Space: 13.8 MB (90.00%) - LeetHub

---
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def largestMultipleOfThree(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: str
+        """
+        dp = [-1,-1,-1]
+        for a in sorted(digits)[::-1]:
+            for x in dp[:] + [0]:
+                y = x * 10 + a
+                dp[y % 3] = max(dp[y % 3], y)
+        return str(dp[0]) if dp[0] >= 0 else ""
+        

f70a6507d68e15b87c24c845a64610ad91444224
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-07 16:37:33 -0500

    Time: 55 ms (88.73%), Space: 16.7 MB (75.00%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -4,13 +4,17 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: bool
         """
-        if len(intervals)<=1:
-            return True
-        intervals.sort(key=lambda x:x[0])
-        temp=[intervals[0]]
-        for start,end in intervals[1:]:
-            if temp[-1][1]>start:
-                return False
-            else:
-                temp.append([start,end])
-        return True
+        
+        # if len(intervals)<=1:
+        #     return True
+        # intervals.sort(key=lambda x:x[0])
+        # temp=[intervals[0]]
+        # for start,end in intervals[1:]:
+        #     if temp[-1][1]>start:
+        #         return False
+        #     else:
+        #         temp.append([start,end])
+        # return True
+        
+        intervals.sort()
+        return all(intervals[i][0] >= intervals[i-1][1] for i in range(1, len(intervals)))

d2a34c32c1949cdba20edb320da61435b6e546da
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-09 19:57:22 -0500

    Time: 350 ms (56.17%), Space: 13.8 MB (83.77%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # str1=''
-        # for i in num:
-        #     str1+=str(i)
-        # answer=int(str1)+k
-        # list1=[]
-        # for i in str(answer):
-        #     list1.append(int(i))
-        # return list1
+        str1=''
+        for i in num:
+            str1+=str(i)
+        answer=int(str1)+k
+        list1=[]
+        for i in str(answer):
+            list1.append(int(i))
+        return list1
     
-        return list(str(int("".join(map(str,num)))+k))
+        #return list(str(int("".join(map(str,num)))+k))

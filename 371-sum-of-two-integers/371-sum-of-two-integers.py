257dff2f13ab6a5f444af5bde58485fed0ed10e0
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-09 19:41:00 -0500

    Time: 27 ms (30.97%), Space: 13.5 MB (12.26%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -5,4 +5,10 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        return sum([a,b])
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+        

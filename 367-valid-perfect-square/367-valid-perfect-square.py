976a1d60f5a42dd0abbb8ff3ade17b430e713fb7
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-16 20:33:31 -0500

    Time: 20 ms (73.06%), Space: 13.3 MB (61.50%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # Binary search
+        if num < 2:
+            return True
+        
+        left, right = 2, num // 2
+        
+        while left <= right:
+            x = left + (right - left) // 2
+            guess_squared = x * x
+            if guess_squared == num:
+                return True
+            if guess_squared > num:
+                right = x - 1
+            else:
+                left = x + 1
+        
+        return False
+    
+        # Newton's method
+        r = x
+        while r*r > x:
+            r = (r + x/r) / 2
+        return r*r == x

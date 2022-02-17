0e31b39bc319a714bcd5819915b545ed13c148e9
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-16 20:10:41 -0500

    Time: 16 ms (96.93%), Space: 13.4 MB (65.23%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # Cheating and using formula sqrt(x)=e^(0.5*log(x))
+        if x < 2:
+            return x
+        
+        left = int(e**(0.5 * log(x)))
+        right = left + 1
+        return left if right * right > x else right

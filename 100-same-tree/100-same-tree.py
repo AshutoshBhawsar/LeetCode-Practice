1787a42de99efc21452d0285ae9b5ca67d6e986d
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-16 20:57:49 -0500

    Time: 24 ms (49.39%), Space: 13.4 MB (68.00%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -11,10 +11,16 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if not q and not p:
-            return True
-        if not p or not q or p.val!=q.val:
-            return False
-        flag1=self.isSameTree(p.left,q.left)
-        flag2=self.isSameTree(p.right,q.right)
-        return flag1 and flag2
+        # Traditional method recursion ##############
+        # if not q and not p:
+        #     return True
+        # if not p or not q or p.val!=q.val:
+        #     return False
+        # flag1=self.isSameTree(p.left,q.left)
+        # flag2=self.isSameTree(p.right,q.right)
+        # return flag1 and flag2
+        
+        # Consolidated version ###########
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q

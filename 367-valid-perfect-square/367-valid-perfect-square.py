7231e4a3ee163d38a7ecba65d3217d859be1ef72
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-16 20:34:07 -0500

    Time: 22 ms (60.08%), Space: 13.5 MB (12.69%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -5,25 +5,25 @@ class Solution(object):
         :rtype: bool
         """
         # Binary search
-        if num < 2:
-            return True
+#         if num < 2:
+#             return True
         
-        left, right = 2, num // 2
+#         left, right = 2, num // 2
         
-        while left <= right:
-            x = left + (right - left) // 2
-            guess_squared = x * x
-            if guess_squared == num:
-                return True
-            if guess_squared > num:
-                right = x - 1
-            else:
-                left = x + 1
+#         while left <= right:
+#             x = left + (right - left) // 2
+#             guess_squared = x * x
+#             if guess_squared == num:
+#                 return True
+#             if guess_squared > num:
+#                 right = x - 1
+#             else:
+#                 left = x + 1
         
-        return False
+#         return False
     
         # Newton's method
-        r = x
-        while r*r > x:
-            r = (r + x/r) / 2
-        return r*r == x
+        r = num
+        while r*r > num:
+            r = (r + num/r) / 2
+        return r*r == num

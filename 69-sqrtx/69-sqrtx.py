baa9d545a2e2633ba4a5a9fea49cf6e61e5c95ea
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-16 20:13:19 -0500

    Time: 36 ms (49.76%), Space: 13 MB (99.25%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -5,9 +5,15 @@ class Solution(object):
         :rtype: int
         """
         # Cheating and using formula sqrt(x)=e^(0.5*log(x))
-        if x < 2:
-            return x
+#         if x < 2:
+#             return x
         
-        left = int(e**(0.5 * log(x)))
-        right = left + 1
-        return left if right * right > x else right
+#         left = int(e**(0.5 * log(x)))
+#         right = left + 1
+#         return left if right * right > x else right
+    
+        # Newton's method
+        r = x
+        while r*r > x:
+            r = (r + x/r) / 2
+        return r

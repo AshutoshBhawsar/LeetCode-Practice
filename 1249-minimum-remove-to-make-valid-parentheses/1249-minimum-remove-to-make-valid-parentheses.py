0f211edb8c0bf08910aca2edd0af5a1a2ea12ae7
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-20 13:32:20 -0500

    Time: 7791 ms (14.24%), Space: 18.5 MB (26.63%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -5,20 +5,20 @@ class Solution(object):
         :rtype: str
         """
         stack=[]
-        list1=[]
+        list1={}
         ans=''
         for i in range(len(s)):
             if s[i]=='(':
                 stack.append(i)
             elif s[i]==')':
                 if not stack:
-                    list1.append(i)
+                    list1[i]=i
                     continue
                 else:
                     stack.pop()
         if stack:
             for i in stack:
-                list1.append(i)
+                list1[i]=i
         for i in range(len(s)):
             if i not in list1:
                 ans+=s[i]

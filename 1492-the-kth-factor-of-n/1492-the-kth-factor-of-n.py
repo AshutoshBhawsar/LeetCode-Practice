21e5ec18ab606fc7438d04c697f07b6e893f1c59
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-20 18:10:53 -0500

    Time: 28 ms (46.34%), Space: 13.5 MB (38.61%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -5,11 +5,25 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        c=0
-        for i in range(1,n+1):
-            if n%i==0:
-                c+=1
-            if k==c:
-                return i
+        # brute force############
+#         c=0
+#         for i in range(1,n+1):
+#             if n%i==0:
+#                 c+=1
+#             if k==c:
+#                 return i
         
-        return -1
+#         return -1
+
+        # math upto sqrt(n) and save 2 divisors in 1 successful division 
+        f1, f2 = [], []
+        for s in range(1, int(sqrt(n)) + 1 ):
+            if n % s == 0:
+                f1 += [s]
+                f2 += [n//s]
+                
+        if f1[-1] == f2[-1]: f2.pop() #perfect square condition hence eliminate 1 entry
+            
+        factors = f1 + f2[::-1]
+        return -1 if len(factors) < k else factors[k-1]
+        

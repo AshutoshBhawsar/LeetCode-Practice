96727e38281d116e4c273a66c8cd39706bfa2762
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-20 17:59:51 -0500

    Time: 30 ms (39.01%), Space: 13.5 MB (13.86%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def kthFactor(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        c=0
+        for i in range(1,n+1):
+            if n%i==0:
+                c+=1
+            if k==c:
+                return i
+        
+        return -1

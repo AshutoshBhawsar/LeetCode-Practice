1a3df614a33419557179c970c3beb9b034c7c22b
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-20 20:52:32 -0500

    Time: 673 ms (69.88%), Space: 19.2 MB (93.70%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -6,8 +6,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         return sorted(points, key= lambda x:self.distance(x))[:k]
-        #print(points)
-        #return [lambda list1[:k] : list1]
     
     def distance(self,x):
         return x[0]**2 + x[1]**2

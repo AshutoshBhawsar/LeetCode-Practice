d5530317371530416b0d49ccbae1d1210678bd66
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-25 18:44:46 -0500

    Time: 37 ms (13.21%), Space: 13.6 MB (17.36%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countPoints(self, rings):
+        """
+        :type rings: str
+        :rtype: int
+        """
+        h = collections.defaultdict(set)
+        for i in range(0, len(rings) - 1, 2):
+            h[rings[i + 1]].add(rings[i])
+        return sum(len(v) == 3 for v in h.values())

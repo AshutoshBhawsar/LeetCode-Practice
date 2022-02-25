d3278aa99491318bcfddfafc8d3bf9ef1b31e99b
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-25 13:38:08 -0500

    Time: 20 ms (88.24%), Space: 13.3 MB (70.54%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -12,5 +12,18 @@ class Solution(object):
         # return nums
         
         # using lib function 'Illegal'
-        return nums.sort()
+        #return nums.sort()
         
+        # dutch partitioning problem
+        red, white, blue = 0, 0, len(nums)-1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1

e1fd5621679f620f9cd7a66243a58345cc457ca5
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-27 17:12:37 -0500

    Time: 24 ms (66.00%), Space: 13.2 MB (86.76%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,6 +6,16 @@ class Solution(object):
         """
         
         # simple recursion
+        # if n<2:
+        #     return n
+        # return self.fib(n-1)+self.fib(n-2)
+    
+    
+        # DP
         if n<2:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        dp=[0]*(n+1)
+        dp[0],dp[1]=0,1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]

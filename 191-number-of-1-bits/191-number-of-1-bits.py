760f517e6edd75a5e0d29b901bba72b6ac8bf22a
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-28 19:46:32 -0500

    Time: 38 ms (16.06%), Space: 13.4 MB (36.71%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # counter
         return collections.Counter(bin(n)[2:])['1']
+    
+        # pop count
+        # sum1=0
+        # while(n!=0):
+        #     sum1+=1
+        #     n&=(n-1)
+        # return sum1

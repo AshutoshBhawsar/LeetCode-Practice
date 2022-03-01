35ca34eda6cb9399b7b8a2ed6511ce082bc9380a
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-02-28 19:36:48 -0500

    Time: 132 ms (34.15%), Space: 17.4 MB (59.65%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -4,8 +4,33 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        ans=[]
-        for i in range(n+1):
-            t=collections.Counter(bin(i)[2:])
-            ans.append(t['1'])
-        return ans
+        # brute force solution #######
+        # ans=[]
+        # for i in range(n+1):
+        #     t=collections.Counter(bin(i)[2:])
+        #     ans.append(t['1'])
+        # return ans
+        
+        
+        # pop count approach ###########
+        def pop_count(x):
+            count = 0
+            while x != 0:
+                x &= x - 1 # zeroing out the least significant nonzero bit
+                count += 1
+            return count
+            
+        ans = [0] * (n + 1)
+        for x in range(n + 1):
+            ans[x] = pop_count(x)
+    
+        return ans  
+
+        
+        # DP ########
+        # ans = [0] * (n + 1)
+        # for x in range(1, n + 1):
+        #     ans[x] = ans[x & (x - 1)] + 1
+        # return ans 
+
+        

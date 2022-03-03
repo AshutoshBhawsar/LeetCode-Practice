cc3ecf722f51ea4444b1543fd1dddca0b424b66e
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-02 21:44:57 -0500

    Time: 26 ms (93.31%), Space: 13.6 MB (14.43%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.py
@@ -10,10 +10,18 @@ class Solution(object):
         :rtype: ListNode
         """
         # simple one pass    
-        temp=head
-        while(temp and temp.next):
-            if temp.val==temp.next.val:
-                temp.next=temp.next.next
-            else:
-                temp=temp.next
+        # temp=head
+        # while(temp and temp.next):
+        #     if temp.val==temp.next.val:
+        #         temp.next=temp.next.next
+        #     else:
+        #         temp=temp.next
+        # return head
+    
+        # Kind of optimised
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next     # skip duplicated node
+            cur = cur.next     # not duplicate of current node, move to next node
         return head

80ed052e517add1526a84b9757de8900d1a17158
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-02 19:37:46 -0500

    Time: 25 ms (76.06%), Space: 13.8 MB (39.39%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [0] * n
+        ans = 0
+        for i in range(2, n):
+            if nums[i-1] - nums[i-2] == nums[i] - nums[i-1]:
+                dp[i] = dp[i-1] + 1
+            ans += dp[i]
+        return ans

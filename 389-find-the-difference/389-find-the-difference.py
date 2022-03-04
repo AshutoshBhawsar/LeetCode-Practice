f1276d6e725a176345d70c1252654e5af3c4abe8
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-04 18:00:05 -0500

    Time: 67 ms (8.61%), Space: 13.7 MB (18.79%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        hmap={x:s.count(x) for x in s}
+        for y in t:
+            if y not in hmap or hmap[y]==0:
+                return y
+            hmap[y]-=1

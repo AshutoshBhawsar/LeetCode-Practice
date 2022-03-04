eb79ac00449d9356df83cc9a47196b843e93ef1f
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-03 21:28:37 -0500

    Time: 23 ms (65.45%), Space: 13.4 MB (76.05%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -10,20 +10,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
+        # iterative simulation #############
+        # if not root:
+        #     return root
+        # queue=[root]
+        # for ele in queue:
+        #     if ele.left and ele.right:
+        #         queue.append(ele.left)
+        #         queue.append(ele.right)
+        #         ele.left,ele.right=ele.right,ele.left
+        #     elif ele.left:
+        #         queue.append(ele.left)
+        #         ele.right=ele.left
+        #         ele.left=None
+        #     elif ele.right:
+        #         queue.append(ele.right)
+        #         ele.left=ele.right
+        #         ele.right=None
+        # return root
+        
+        # Crazy recursive ! #################
         if not root:
             return root
-        queue=[root]
-        for ele in queue:
-            if ele.left and ele.right:
-                queue.append(ele.left)
-                queue.append(ele.right)
-                ele.left,ele.right=ele.right,ele.left
-            elif ele.left:
-                queue.append(ele.left)
-                ele.right=ele.left
-                ele.left=None
-            elif ele.right:
-                queue.append(ele.right)
-                ele.left=ele.right
-                ele.right=None
+        root.left, root.right=self.invertTree(root.right), self.invertTree(root.left)
         return root

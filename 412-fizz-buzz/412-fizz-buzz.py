1b1fbf34c1d2ae4f248087c579f5a165a7acc849
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-03 22:29:45 -0500

    Time: 56 ms (13.36%), Space: 14.3 MB (15.06%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return [('Fizz' if i % 3 == 0 else '') + ('Buzz' if i % 5 == 0 else '') + (str(i) if not (i % 3 == 0 or i % 5 == 0) else '') for i in range(1,n+1)]

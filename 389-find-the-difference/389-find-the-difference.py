a945156d24604fefff4b0ad72087eaa77ff94a72
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-04 18:02:41 -0500

    Time: 42 ms (36.73%), Space: 13.6 MB (18.79%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        hmap={x:s.count(x) for x in s}
-        for y in t:
-            if y not in hmap or hmap[y]==0:
-                return y
-            hmap[y]-=1
+        # hmap={x:s.count(x) for x in s}
+        # for y in t:
+        #     if y not in hmap or hmap[y]==0:
+        #         return y
+        #     hmap[y]-=1
+        
+        return (Counter(t) - Counter(s)).popitem()[0]

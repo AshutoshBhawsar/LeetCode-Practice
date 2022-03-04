8b2efde8e8a734f20ddb3cfee991a2f1dae26a0b
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-04 18:33:44 -0500

    Time: 11 ms (100.00%), Space: 13.5 MB (31.61%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.py
+++ 441-arranging-coins/441-arranging-coins.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        count=0
-        i=1
-        while n>=0:
-            n-=i
-            count+=1
-            i+=1
-        return count-1
+        # count=0
+        # i=1
+        # while n>=0:
+        #     n-=i
+        #     count+=1
+        #     i+=1
+        # return count-1
+        
+        # Do your math homework !!!
+        return int(-0.5+(sqrt(2*n+0.25)))
             

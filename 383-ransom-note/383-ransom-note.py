bcdaf6d90b13431e0c6ba85334869c27d8b42c92
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-04 17:48:03 -0500

    Time: 115 ms (38.39%), Space: 13.6 MB (82.57%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -5,8 +5,17 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        rcounts,mcounts=collections.Counter(ransomNote),collections.Counter(magazine)
+        # rcounts,mcounts=collections.Counter(ransomNote),collections.Counter(magazine)
+        # for i in ransomNote:
+        #     if rcounts[i]>mcounts[i]:
+        #         return False
+        # return True
+        
+        if len(ransomNote)>len(magazine):
+            return False
+        counts=collections.Counter(magazine)
         for i in ransomNote:
-            if rcounts[i]>mcounts[i]:
+            if counts[i]<=0:
                 return False
+            counts[i]-=1
         return True

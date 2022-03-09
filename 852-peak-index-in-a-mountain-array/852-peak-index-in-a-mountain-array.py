d57d09eb7f029873c6b49364267fbbf61eaf0d1e
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-09 13:36:34 -0500

    Time: 71 ms (61.34%), Space: 14.7 MB (15.78%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        for i in range(1,len(arr)-1):
-            if arr[i]>arr[i+1]:
-                return i
+        # for i in range(1,len(arr)-1):
+        #     if arr[i]>arr[i+1]:
+        #         return i
+        
+        l, r = 0, len(arr) - 1
+        while l < r:
+            m = (l + r) / 2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l

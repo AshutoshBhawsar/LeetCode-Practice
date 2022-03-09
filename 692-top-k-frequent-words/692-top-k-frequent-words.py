cb839926ffb073087911842cd20d3306584ce70e
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-03-09 16:53:52 -0500

    Time: 74 ms (25.99%), Space: 13.9 MB (8.66%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        counts = collections.Counter(words)
+        return heapq.nsmallest(k, counts, key=lambda word:(~counts[word], word))
+        

1dd0a8ab85c714089e60134c916204c0e4f232c5
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-04-04 12:16:46 -0400

    Time: 1917 ms (34.73%), Space: 91.1 MB (67.85%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -10,16 +10,38 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
-        list1=[]
+        # Extra memory
+        # list1=[]
+        # temp=head
+        # while(temp):
+        #     list1.append(temp.val)
+        #     temp=temp.next
+        # list1[k-1],list1[-k]=list1[-k],list1[k-1]
+        # newtemp=ListNode(0)
+        # temp=newtemp
+        # for i in list1:
+        #     newNode=ListNode(i)
+        #     temp.next=newNode
+        #     temp=temp.next
+        # return newtemp.next
+        
+        # in place
+        temp=head
+        place1,place2=None,None
+        count=0
+        while(temp):
+            count+=1
+            temp=temp.next
+        #print (count)
+        i=0
         temp=head
         while(temp):
-            list1.append(temp.val)
+            #print(i)
+            if k-1==i:
+                place1=temp
+            if count-k==i:
+                place2=temp
+            i+=1
             temp=temp.next
-        list1[k-1],list1[-k]=list1[-k],list1[k-1]
-        newtemp=ListNode(0)
-        temp=newtemp
-        for i in list1:
-            newNode=ListNode(i)
-            temp.next=newNode
-            temp=temp.next
-        return newtemp.next
+        place1.val,place2.val=place2.val,place1.val
+        return head

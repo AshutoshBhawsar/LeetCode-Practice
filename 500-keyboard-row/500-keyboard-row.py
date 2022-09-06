4af3736d3d0c14895b46f8965698ee64720f69ec
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-09-06 09:07:36 -0700

    Time: 41 ms (70.32%), Space: 13.9 MB (68.73%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.py
+++ 500-keyboard-row/500-keyboard-row.py
@@ -1,10 +1,25 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        return filter(lambda word:
-                        set(word.lower()) - set("qwertyuiop") == set() or
-                        set(word.lower()) - set("asdfghjkl") == set() or
-                        set(word.lower()) - set("zxcvbnm") == set(),
-                    words) 
+        # Set approach
+        line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+        ret = []
+        for word in words:
+          w = set(word.lower())
+          if w <= line1 or w <= line2 or w <= line3:
+            ret.append(word)
+        return ret
+        
+        
+        
+        
+        
+        
+        # One liner
+#         return filter(lambda word:
+#                         set(word.lower()) - set("qwertyuiop") == set() or
+#                         set(word.lower()) - set("asdfghjkl") == set() or
+#                         set(word.lower()) - set("zxcvbnm") == set(),
+#                     words) 
                 
                 
                     

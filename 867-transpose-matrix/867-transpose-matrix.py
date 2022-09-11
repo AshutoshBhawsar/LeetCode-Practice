1ca5da9ff44501d6c1313fc349806213bc964faa
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-09-11 14:30:56 -0700

    Time: 147 ms (5.59%), Space: 14.2 MB (81.12%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def transpose(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m,n=len(matrix),len(matrix[0])
+        transpose=[[0 for i in range(m)] for j in range(n)]
+        #print(transpose)
+        for i in range(m):
+            for j in range(n):
+                transpose[j][i]=matrix[i][j]
+        return transpose

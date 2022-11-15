f75ba9a57c43e08fa023f9dbb3e01c966df4fd73
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-11-15 13:27:58 -0500

    Time: 166 ms (96.63%), Space: 15.6 MB (17.46%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Brute force extra memory, worst solution possible
+        arr=[]
+        for num in nums:
+            if num!=0:
+                arr.append(num)
+        n,a=len(nums),len(arr)
+        for i in range(n):
+            if i<a:
+                nums[i]=arr[i]
+            else:
+                nums[i]=0
+        

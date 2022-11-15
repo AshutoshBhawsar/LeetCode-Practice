b54488c5701359316dae85b31956f5d907b770f8
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2022-11-15 13:34:12 -0500

    Time: 293 ms (75.72%), Space: 15.7 MB (17.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,14 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # Brute force extra memory, worst solution possible
-        arr=[]
-        for num in nums:
-            if num!=0:
-                arr.append(num)
-        n,a=len(nums),len(arr)
-        for i in range(n):
-            if i<a:
-                nums[i]=arr[i]
-            else:
-                nums[i]=0
+#         arr=[]
+#         for num in nums:
+#             if num!=0:
+#                 arr.append(num)
+#         n,a=len(nums),len(arr)
+#         for i in range(n):
+#             if i<a:
+#                 nums[i]=arr[i]
+#             else:
+#                 nums[i]=0
         
+        # Optimized swapping 2 pointers, O(n), O(1)
+        zero=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[zero]=nums[zero],nums[i]
+                zero+=1

a21b4c2140215fdba28d7c2b40c3f10aa656d090
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2023-01-01 18:29:24 +0530

    Time: 144 ms (78.63%), Space: 17.2 MB (5.16%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Brute Force
-        counts=collections.Counter(nums)
-        for key,value in counts.items():
-            if value==1:
-                return key
-            
+        # counts=collections.Counter(nums)
+        # for key,value in counts.items():
+        #     if value==1:
+        #         return key
+        
+        # Math Magic
+        return 2*sum(set(nums))-sum(nums)
+        

9644d75e28b907f7139039b86b585c6f76af6996
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2023-01-01 23:29:48 +0530

    Time: 186 ms (97.55%), Space: 15.9 MB (38.77%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,12 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        seen=set()
-        n=len(nums)
-        for num in nums:
-            if num in seen:
-                repeat=num
-            seen.add(num)
-        return [repeat,sum(range(n+1))-sum(nums)+repeat]
+        # My normal map approach
+        # seen=set()
+        # n=len(nums)
+        # for num in nums:
+        #     if num in seen:
+        #         repeat=num
+        #     seen.add(num)
+        # return [repeat,sum(range(n+1))-sum(nums)+repeat]
             
-            
-        
+        # One liner by Lord Stefan Pochmann
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]

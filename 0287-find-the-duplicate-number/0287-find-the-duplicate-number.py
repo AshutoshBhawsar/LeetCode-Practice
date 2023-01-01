0766dd8671804100f32c1ac0baf87a438cb900bc
Ashutosh Shailesh Bhawsar <ashutoshbhawsar98@gmail.com>
2023-01-01 18:59:25 +0530

    Time: 696 ms (74.08%), Space: 27.9 MB (91.62%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # Brute Force
-        counts=collections.Counter(nums)
-        for key,value in counts.items():
-            if value>=2:
-                return key
+        # counts=collections.Counter(nums)
+        # for key,value in counts.items():
+        #     if value>=2:
+        #         return key
+        
+        # Cycle detection
+        for ele in nums:
+            if nums[abs(ele)]<0:
+                return abs(ele)
+            nums[abs(ele)]*=-1
         
